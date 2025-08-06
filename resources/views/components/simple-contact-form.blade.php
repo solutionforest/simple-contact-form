25b8044d676571044f0c23870f761cac4dc4a36b
haywardTsoi <hayward.tsoi@solutionforest.net>
2025-08-06 17:50:53 +0800

    fix

--- resources/views/components/simple-contact-form.blade.php
+++ resources/views/components/simple-contact-form.blade.php
@@ -2,16 +2,9 @@
 <div class="simple-contact-form-wrapper w-auto">
     @once
         
-        @if(file_exists(public_path('build/assets/index.css')) || file_exists(public_path('css/simple-contact-form.css')))
-            
-            @if(file_exists(public_path('css/simple-contact-form.css')))
-                <link rel="stylesheet" href="{{ asset('css/simple-contact-form.css') }}">
-            @else
-                @vite(['resources/css/app.css'])
-            @endif
-        @else
+      
             <link rel="stylesheet" href="{{ asset('vendor/simple-contact-form/simple-contact-form.css') }}">
-        @endif
+     
         @livewireStyles
         @filamentStyles
         @livewire('notifications')
